676cd28a58ddca1be85c9b6c133e51548668b2a7
Xtemp00 <b.rondo@orange.fr>
2022-03-17 21:41:31 +0100

    ajout d'une fonctionnalité permettant de trouver sa localisation dans la page settings, puis ajout d'un fichier javascript pour l'index et enfin ajout d'un fichier json pour effectuer des test

--- javascript/settings.js
+++ javascript/settings.js
@@ -4,7 +4,38 @@ function email() {
         window.open("Scylla.html")
     }
 }
-const fs = require('fs')
-let fichier = fs.readFileSync('données/recup_données.json')
-let personne = JSON.parse(fichier)
-console.log(personne)
+
+
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Impossible d avoir votre localisation';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocalisation est impossible sur votre navigateur';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+
